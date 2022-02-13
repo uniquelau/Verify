849568d3b9711dcdb3f554729012b53263818b64
Simon Cropp <simon.cropp@gmail.com>
2022-02-13 12:58:12 +1100

    Update Tests.cs

--- src/Verify.NUnit.Tests/Tests.cs
+++ src/Verify.NUnit.Tests/Tests.cs
@@ -14,4 +14,17 @@ public class Tests
         return Verify(arg)
             .UseTextForParameters("TextForParameter");
     }
+
+    static IEnumerable<int> testCases = Enumerable.Range(0, 3600);
+
+    [TestCaseSource(nameof(testCases))]
+    public Task ManyTestCases(int testCase)
+    {
+        var result = testCase * 2;
+        return Verify(result)
+            .UseParameters(testCase)
+            .UseDirectory("snaps")
+            .AutoVerify()
+            .DisableDiff();
+    }
 }
